2c4e5fd0a4e1919d4fc73f1213c8e303c6c72648
王乃强 <sadwadf@qq.com>
2019-02-15 11:59:02 +0800

    搜索框

--- src/app/task-list/task-list.component.ts
+++ src/app/task-list/task-list.component.ts
@@ -11,12 +11,20 @@ import { NzMessageService } from 'ng-zorro-antd';
 })
 export class TaskListComponent implements OnInit {
   mylist = [];
+  /** 搜索内容 */
+  search = '';
+  /** 总条数 */
+  TotalCount = 0;
+  /** 列表加载loading */
+  listloading = true;
   /** 请求任务列表接口所需的数据 */
   GAT: GetAllotTasksModel = {
     // search: '',
     pageIndex: 0,
-    pageSize: 100
+    pageSize: 10
   };
+  /** 列表绑定页数，因为后台传回来的页数是从0开始的 */
+  pageIndex = 1;
 
   constructor(
     public service: Service,
@@ -28,11 +36,24 @@ export class TaskListComponent implements OnInit {
     this.GetAllotTasks();
   }
 
+  /** 搜索 */
+  listsearch(search: string) {
+    this.GAT.search = search;
+    this.pageIndex = 1;
+    console.log(this.GAT.search);
+    this.GetAllotTasks();
+  }
+
   /** 获取任务列表 */
   GetAllotTasks() {
+    this.listloading = true;
+    this.GAT.pageIndex = this.pageIndex;
+    --this.GAT.pageIndex;
     this.service.GetAllotTasks(this.GAT).subscribe(r => {
       if (r.Status) {
         this.mylist = r.Info.Data;
+        this.TotalCount = r.Info.TotalCount;
+        this.listloading = false;
       }
     });
   }
@@ -56,6 +77,10 @@ export class TaskListComponent implements OnInit {
   delete(Id: string) {
     this.service.DeleteAllotTask({ allotTaskId: Id }).subscribe(r => {
       if (r.Status) {
+        // 当前页数据删完返回上一页
+        if (this.mylist.length === 1) {
+          this.pageIndex--;
+        }
         this.GetAllotTasks();
       } else {
         this.message.create('error', '删除失败');
